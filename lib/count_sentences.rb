b5c2f0f89c10948724c1c64c22a7e021fbd38b2e
Benjamin-Coleman <github email address>
2017-06-28 18:14:03 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self[-1] == "."
+    def end_with?(last_character)
+    	self[-1] = last_character
+    end
   end
 
   def question?
-
+  	return self[-1] == "?"
   end
 
   def exclamation?
-
+  	return self[-1] == "!"
   end
 
   def count_sentences
-
+  	self.squeeze.split(/[.!?]/).count
+  	#binding.pry
   end
 end
